52dc497f5b599c3d4c2eb08648b95a597f8432b6
IBAD ULLAH KHAN <ibad50008@gmail.com>
2024-11-02 13:20:29 +0500

    python
    started the practice of the python

---
+++ practice 01/file.py
@@ -0,0 +1,9 @@
+print("hello i m learning the python")
+name =input("Enter your name: ")
+print ("my name is : ",name)
+age=int(input("Enter your age: "))
+print ("your age is : ",age)
+if age<=18:
+ print("I m not ibad")
+else :
+ print("hello")
